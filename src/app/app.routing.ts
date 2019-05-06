14cc03b5786c8af0d5ce0412a5daae6a1d9fe354
bjlkak <bjlkak@hotmail.com>
2019-05-06 00:14:12 -0700

    final2

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { Work1Component } from './work1/work1.component';
 import { Work2Component } from './work2/work2.component';
 import { Work3Component } from './work3/work3.component';
 import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
 
 export const appRoutes: Routes = [  
   { path: 'p1', component: Work1Component },
@@ -12,3 +15,9 @@ export const appRoutes: Routes = [
   { path: '', redirectTo: 'p1', pathMatch: 'full' },
   { path: '**', redirectTo: 'p1', pathMatch: 'full' }
 ];
+
+@NgModule({
+  imports:[RouterModule.forRoot(appRoutes,{useHash:true})],
+  exports: [RouterModule]
+})
+export class AppModule{}
